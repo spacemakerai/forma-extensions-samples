790d8a8413bb7d237d540277473ba1db8c023e48
Håvard Høiby <havard.hoiby@autodesk.com>
2023-08-29 19:21:55 +0200

    feat: update the styling of the sun study extensions

--- analyses/sun-study/index.js
+++ analyses/sun-study/index.js
@@ -1,36 +1,87 @@
 import formaApi from "forma-sdk";
 
-const monthInput = document.getElementById("month");
-const dayInput = document.getElementById("day");
-const startInput = document.getElementById("start");
-const endInput = document.getElementById("end");
+const timeindicator = document.getElementById("timeindicator");
 
-const btn = document.getElementById("run");
-btn.addEventListener("click", async () => {
+function updateTimeIndicator() {
+  const startHour = parseInt(document.getElementById("startHour").value, 10);
+  const startMinute = parseInt(
+    document.getElementById("startMinute").value,
+    10
+  );
+  const endHour = parseInt(document.getElementById("endHour").value, 10);
+  const endMinute = parseInt(document.getElementById("endMinute").value, 10);
+
+  const width = 212;
+
+  const startMinutes = startHour * 60 + startMinute;
+  const endMinutes = endHour * 60 + endMinute;
+
+  const startPos = Math.floor((startMinutes * width) / (24 * 60));
+  const endPos = Math.floor((endMinutes * width) / (24 * 60));
+  const lineLength = Math.floor(
+    ((endMinutes - startMinutes) * width) / (24 * 60)
+  );
+
+  const [preLineNode] = timeindicator.getElementsByClassName("pre-line");
+  preLineNode.style.width = startPos + "px";
+  const [lineNode] = timeindicator.getElementsByClassName("line");
+  lineNode.style.width = lineLength + "px";
+  const [postLineNode] = timeindicator.getElementsByClassName("post-line");
+  postLineNode.style.width = width - endPos + "px";
+}
+
+updateTimeIndicator();
+
+document.getElementById("startHour").onchange = updateTimeIndicator;
+document.getElementById("startMinute").onchange = updateTimeIndicator;
+document.getElementById("endHour").onchange = updateTimeIndicator;
+document.getElementById("endMinute").onchange = updateTimeIndicator;
+document.getElementById("interval").onchange = updateTimeIndicator;
+
+document.getElementById("run").onclick = async () => {
   const existingDate = await formaApi.ui.sun.get();
 
-  const month = parseInt(monthInput.value, 10);
-  const day = parseInt(dayInput.value, 10);
-  const start = parseInt(startInput.value, 10);
-  const end = parseInt(endInput.value, 10);
+  const month = parseInt(document.getElementById("month").value, 10);
+  const day = parseInt(document.getElementById("day").value, 10);
+  const startHour = parseInt(document.getElementById("startHour").value, 10);
+  const startMinute = parseInt(
+    document.getElementById("startMinute").value,
+    10
+  );
+  const endHour = parseInt(document.getElementById("endHour").value, 10);
+  const endMinute = parseInt(document.getElementById("endMinute").value, 10);
 
-  const d = new Date();
-  d.setMonth(month);
-  d.setDate(day);
-  d.setMinutes(0);
-  d.setSeconds(0);
+  const interval = parseInt(document.getElementById("interval").value, 10);
+  const resolution = document.getElementById("resolution").value;
+  const width = parseInt(resolution.split("x")[0], 10);
+  const height = parseInt(resolution.split("x")[1], 10);
 
-  for (let i = start; i <= end; i++) {
-    d.setHours(i);
-    await formaApi.ui.sun.set(d);
-    await saveScreen(d.toString());
+  const startDate = new Date();
+  startDate.setMonth(month);
+  startDate.setDate(day);
+  startDate.setHours(startHour);
+  startDate.setMinutes(startMinute);
+  startDate.setSeconds(0);
+
+  const endDate = new Date();
+  endDate.setMonth(month);
+  endDate.setDate(day);
+  endDate.setHours(endHour);
+  endDate.setMinutes(endMinute);
+  endDate.setSeconds(0);
+
+  while (startDate.getTime() <= endDate.getTime()) {
+    await formaApi.ui.sun.set(startDate);
+    await saveScreen(startDate.toString(), width, height);
+
+    startDate.setTime(startDate.getTime() + interval * 60 * 1000);
   }
 
   await formaApi.ui.sun.set(existingDate);
-});
+};
 
-async function saveScreen(name) {
-  const url = await formaApi.ui.camera.capture(1024, 768);
+async function saveScreen(name, width, height) {
+  const url = await formaApi.ui.camera.capture(width, height);
 
   const save_link = document.createElement("a");
   save_link.href = url;
