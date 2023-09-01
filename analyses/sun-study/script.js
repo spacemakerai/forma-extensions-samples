7e0de07bfdc3d44bc76dc99e205fe624e7ffabce
Håvard Høiby <havard.hoiby@autodesk.com>
2023-09-01 08:27:46 +0200

    feat: update sun-study example

--- analyses/sun-study/script.js
+++ analyses/sun-study/script.js
@@ -1,4 +1,10 @@
-import formaApi from "forma-sdk";
+import { EmbeddedViewSdk } from "./forma-embedded-view-sdk/index.js";
+import { SunApi } from "./forma-embedded-view-sdk/sun.js";
+import { CameraApi } from "./forma-embedded-view-sdk/camera.js";
+const { origin } = EmbeddedViewSdk.parseHostParams();
+const sdk = new EmbeddedViewSdk({ origin });
+const sunApi = new SunApi(sdk);
+const cameraApi = new CameraApi(sdk);
 
 const timeindicator = document.getElementById("timeindicator");
 
@@ -39,52 +45,56 @@ document.getElementById("endMinute").onchange = updateTimeIndicator;
 document.getElementById("interval").onchange = updateTimeIndicator;
 
 document.getElementById("run").onclick = async () => {
-  const existingDate = await formaApi.ui.sun.get();
+  try {
+    const existingDate = await sunApi.getDate();
 
-  const month = parseInt(document.getElementById("month").value, 10);
-  const day = parseInt(document.getElementById("day").value, 10);
-  const startHour = parseInt(document.getElementById("startHour").value, 10);
-  const startMinute = parseInt(
-    document.getElementById("startMinute").value,
-    10
-  );
-  const endHour = parseInt(document.getElementById("endHour").value, 10);
-  const endMinute = parseInt(document.getElementById("endMinute").value, 10);
+    const month = parseInt(document.getElementById("month").value, 10);
+    const day = parseInt(document.getElementById("day").value, 10);
+    const startHour = parseInt(document.getElementById("startHour").value, 10);
+    const startMinute = parseInt(
+      document.getElementById("startMinute").value,
+      10
+    );
+    const endHour = parseInt(document.getElementById("endHour").value, 10);
+    const endMinute = parseInt(document.getElementById("endMinute").value, 10);
 
-  const interval = parseInt(document.getElementById("interval").value, 10);
-  const resolution = document.getElementById("resolution").value;
-  const width = parseInt(resolution.split("x")[0], 10);
-  const height = parseInt(resolution.split("x")[1], 10);
+    const interval = parseInt(document.getElementById("interval").value, 10);
+    const resolution = document.getElementById("resolution").value;
+    const width = parseInt(resolution.split("x")[0], 10);
+    const height = parseInt(resolution.split("x")[1], 10);
 
-  const startDate = new Date();
-  startDate.setMonth(month);
-  startDate.setDate(day);
-  startDate.setHours(startHour);
-  startDate.setMinutes(startMinute);
-  startDate.setSeconds(0);
+    const startDate = new Date();
+    startDate.setMonth(month);
+    startDate.setDate(day);
+    startDate.setHours(startHour);
+    startDate.setMinutes(startMinute);
+    startDate.setSeconds(0);
 
-  const endDate = new Date();
-  endDate.setMonth(month);
-  endDate.setDate(day);
-  endDate.setHours(endHour);
-  endDate.setMinutes(endMinute);
-  endDate.setSeconds(0);
+    const endDate = new Date();
+    endDate.setMonth(month);
+    endDate.setDate(day);
+    endDate.setHours(endHour);
+    endDate.setMinutes(endMinute);
+    endDate.setSeconds(0);
 
-  while (startDate.getTime() <= endDate.getTime()) {
-    await formaApi.ui.sun.set(startDate);
-    await saveScreen(startDate.toString(), width, height);
+    while (startDate.getTime() <= endDate.getTime()) {
+      await sunApi.setDate({ date: startDate });
+      await saveScreen(startDate.toString(), width, height);
 
-    startDate.setTime(startDate.getTime() + interval * 60 * 1000);
+      startDate.setTime(startDate.getTime() + interval * 60 * 1000);
+    }
+
+    await sunApi.setDate({ date: existingDate });
+  } catch (e) {
+    console.log(e);
   }
-
-  await formaApi.ui.sun.set(existingDate);
 };
 
 async function saveScreen(name, width, height) {
-  const url = await formaApi.ui.camera.capture(width, height);
+  const canvas = await cameraApi.capture({ width, height });
 
   const save_link = document.createElement("a");
-  save_link.href = url;
+  save_link.href = canvas.toDataURL("image/png");
   save_link.download = name + ".png";
   const event = new MouseEvent("click", { bubbles: false, cancelable: false });
   save_link.dispatchEvent(event);
