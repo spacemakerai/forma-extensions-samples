1417c3a2be721fcbf37c7080a3475cfe5a43e35d
knut-sverdrup-adsk <knut.sverdrup@autodesk.com>
2023-09-11 21:53:44 +0200

    prettier and time indicator etc

--- analyses/sun-study/src/app.tsx
+++ analyses/sun-study/src/app.tsx
@@ -1,63 +1,70 @@
-import { CalendarIcon } from './assets/CalendarIcon'
-import { Forma } from "forma-embedded-view-sdk/auto"
-import { useState } from 'preact/hooks'
-import _ from 'lodash'
+import { CalendarIcon } from "./assets/CalendarIcon";
+import { Forma } from "forma-embedded-view-sdk/auto";
+import { useState } from "preact/hooks";
+import _ from "lodash";
 
-const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const INDICATOR_WIDTH = 212;
 
-async function saveScreen(name: string, width: number, height: number) {
+const minutesToPixels = (minutes: number) => {
+  return Math.floor((minutes * INDICATOR_WIDTH) / (24 * 60));
+};
+
+async function saveScreen(name: string, resolution: string) {
+  const width = parseInt(resolution.split("x")[0], 10);
+  const height = parseInt(resolution.split("x")[1], 10);
   const canvas = await Forma.camera.capture({ width, height });
 
-  const save_link = document.createElement("a");
-  save_link.href = canvas.toDataURL("image/png");
-  save_link.download = name + ".png";
+  const downloadLink = document.createElement("a");
+  downloadLink.href = canvas.toDataURL("image/png");
+  downloadLink.download = name + ".png";
   const event = new MouseEvent("click", { bubbles: false, cancelable: false });
-  save_link.dispatchEvent(event);
+  downloadLink.dispatchEvent(event);
 }
 
 export function App() {
-  const [month, setMonth] = useState(6)
-  const [day, setDay] = useState(15)
-  const [interval, setInterval] = useState(60)
-  const [startHour, setStartHour] = useState(8)
-  const [startMinute, setStartMinute] = useState(0)
-  const [endHour, setEndHour] = useState(20)
-  const [endMinute, setEndMinute] = useState(0)
-  const [resolution, setResolution] = useState("2048x1536")
+  const [month, setMonth] = useState(6);
+  const [day, setDay] = useState(15);
+  const [interval, setInterval] = useState(60);
+  const [startHour, setStartHour] = useState(8);
+  const [startMinute, setStartMinute] = useState(0);
+  const [endHour, setEndHour] = useState(20);
+  const [endMinute, setEndMinute] = useState(0);
+  const [resolution, setResolution] = useState("2048x1536");
 
-  const onCLickRun = async () => {
+  const startMins = startHour * 60 + startMinute;
+  const endMins = endHour * 60 + endMinute;
+  const numMins = endMins - startMins;
+
+  const onClickExport = async () => {
     try {
-      const existingDate = await Forma.sun.getDate();
-
-      const width = parseInt(resolution.split("x")[0], 10);
-      const height = parseInt(resolution.split("x")[1], 10);
-
-      const startDate = new Date();
-      startDate.setMonth(month);
-      startDate.setDate(day);
-      startDate.setHours(startHour);
-      startDate.setMinutes(startMinute);
-      startDate.setSeconds(0);
-
-      const endDate = new Date();
-      endDate.setMonth(month);
-      endDate.setDate(day);
-      endDate.setHours(endHour);
-      endDate.setMinutes(endMinute);
-      endDate.setSeconds(0);
+      const currentDate = await Forma.sun.getDate();
+      const startDate = new Date(currentDate.getFullYear(), month, day, startHour, startMinute, 0, 0);
+      const endDate = new Date(currentDate.getFullYear(), month, day, endHour, endMinute, 0, 0);
 
       while (startDate.getTime() <= endDate.getTime()) {
         await Forma.sun.setDate({ date: startDate });
-        await saveScreen(startDate.toString(), width, height);
-
+        await saveScreen(startDate.toString(), resolution);
         startDate.setTime(startDate.getTime() + interval * 60 * 1000);
       }
-
-      await Forma.sun.setDate({ date: existingDate });
+      await Forma.sun.setDate({ date: currentDate });
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <>
@@ -68,54 +75,78 @@ export function App() {
       <div class={"section"}>
         <CalendarIcon />
         <div class="selectors">
-          <select class="month" value={month} onChange={e => setMonth(e.target.value)}>
-            {MONTHS.map((name, value) => <option value={value}>{name}</option>)}
+          <select class="month" value={month} onChange={(event) => setMonth(parseInt(event.target.value, 10))}>
+            {MONTHS.map((name, value) => (
+              <option value={value}>{name}</option>
+            ))}
           </select>
-          <select class="date" value={day} onChange={e => setDay(e.target.value)}>
-            {_.range(1, 31).map(value => <option value={value}>{value}</option>)}
+          <select class="date" value={day} onChange={(event) => setDay(parseInt(event.target.value, 10))}>
+            {_.range(1, 31).map((value) => (
+              <option value={value}>{value}</option>
+            ))}
           </select>
         </div>
       </div>
 
-
       <div class="section">
-        <select id="interval" class="large" value={interval} onChange={e => setInterval(e.target.value)}>
+        <select
+          id="interval"
+          class="large"
+          value={interval}
+          onChange={(event) => setInterval(parseInt(event.target.value, 10))}
+        >
           <option value="5">Image every 5th minute</option>
           <option value="15">Image every 15th minute</option>
           <option value="30">Image every 30th minute</option>
-          <option value="60" selected>Image every hour</option>
+          <option value="60">Image every hour</option>
           <option value="120">Image every 2nd hour</option>
         </select>
       </div>
 
       <div class="section">
-        <select class="timepicker" value={startHour} onChange={e => setStartHour(e.target.value)}>
-          {_.range(25).map(value => <option value={value}>{value < 10 ? "0" + value : value}</option>)}
+        <select
+          class="timepicker"
+          value={startHour}
+          onChange={(event) => setStartHour(parseInt(event.target.value, 10))}
+        >
+          {_.range(25).map((value) => (
+            <option value={value}>{value < 10 ? "0" + value : value}</option>
+          ))}
         </select>
-        <select class="timepicker" value={startMinute} onChange={e => setStartMinute(e.target.value)}>
-          {_.range(60).map(value => <option value={value}>{value < 10 ? "0" + value : value}</option>)}
+        <select
+          class="timepicker"
+          value={startMinute}
+          onChange={(event) => setStartMinute(parseInt(event.target.value, 10))}
+        >
+          {_.range(60).map((value) => (
+            <option value={value}>{value < 10 ? "0" + value : value}</option>
+          ))}
         </select>
         -
-        <select class="timepicker" value={endHour} onChange={e => setEndHour(e.target.value)}>
-          {_.range(25).map(value => <option value={value}>{value < 10 ? "0" + value : value}</option>)}
+        <select class="timepicker" value={endHour} onChange={(event) => setEndHour(parseInt(event.target.value, 10))}>
+          {_.range(25).map((value) => (
+            <option value={value}>{value < 10 ? "0" + value : value}</option>
+          ))}
         </select>
-        <select class="timepicker" value={endMinute} onChange={e => setEndMinute(e.target.value)}>
-          {_.range(60).map(value => <option value={value}>{value < 10 ? "0" + value : value}</option>)}
+        <select class="timepicker" value={endMinute} onChange={(event) => setEndMinute(parseInt(event.target.value))}>
+          {_.range(60).map((value) => (
+            <option value={value}>{value < 10 ? "0" + value : value}</option>
+          ))}
         </select>
       </div>
 
       <div class="section">
         <div class="timeindicator" id="timeindicator">
-          <div class="pre-line"></div>
+          <div class="pre-line" style={{ width: minutesToPixels(startMins) + "px" }}></div>
           <div class="start"></div>
-          <div class="line"></div>
+          <div class="line" style={{ width: minutesToPixels(numMins) + "px" }}></div>
           <div class="end"></div>
-          <div class="post-line"></div>
+          <div class="post-line" style={{ width: INDICATOR_WIDTH - minutesToPixels(endMins) + "px" }}></div>
         </div>
       </div>
 
       <div class="section">
-        <select class="large" value={resolution} onChange={e => setResolution(e.target.value)}>
+        <select class="large" value={resolution} onChange={(event) => setResolution(event.target.value)}>
           <option value="512x384">Small (512x384)</option>
           <option value="1024x768">Medium (1024x768)</option>
           <option value="2048x1536">Large (2048x1536)</option>
@@ -124,8 +155,10 @@ export function App() {
       </div>
 
       <div class="section">
-        <button class="export" onClick={onCLickRun}>Export Images</button>
+        <button class="export" onClick={onClickExport}>
+          Export Images
+        </button>
       </div>
     </>
-  )
+  );
 }
