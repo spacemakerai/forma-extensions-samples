42abc35afa99b81ea0abc3cd358fbc88c0dbb18d
Håvard Høiby <havard.hoiby@autodesk.com>
2023-09-20 19:37:44 +0200

    fix: make the update in scene logic more robus

--- analyses/constraints/src/hooks/useVisualize.js
+++ analyses/constraints/src/hooks/useVisualize.js
@@ -1,15 +1,10 @@
 import { Forma } from "https://esm.sh/forma-embedded-view-sdk/auto";
-import { useEffect } from "https://esm.sh/preact/hooks";
+import { useEffect, useState } from "https://esm.sh/preact/hooks";
 
 import { generateGeometry } from "../util/render.js";
 
-const ids = {};
-
 export function useVisualize(constaintId, runResult, isHovering) {
-  if (!ids[constaintId]) {
-    ids[constaintId] = new Set();
-  }
-
+  const [inScene, setInScene] = useState([]);
   useEffect(async () => {
     if (runResult.type === "success") {
       const failedVisualizations = runResult.data?.info?.outputs?.find(
@@ -19,18 +14,20 @@ export function useVisualize(constaintId, runResult, isHovering) {
       if (failedVisualizations?.value) {
         const color = isHovering ? [0, 255, 0, 255] : [255, 0, 0, 200];
         await Forma.render.updateMesh({
-          id: failedVisualizations.Id,
+          id: constaintId,
           geometryData: await generateGeometry(failedVisualizations, color),
         });
-        ids[constaintId].add(failedVisualizations.Id);
+        setInScene((inScene) => [...inScene, constaintId]);
       } else {
-        if (failedVisualizations?.Id) {
-          await Forma.render.removeMesh({ id: failedVisualizations.Id });
+        if (inScene.includes(constaintId)) {
+          setInScene((inScene) => inScene.filter((id) => id !== constaintId));
+          await Forma.render.remove({ id: constaintId });
         }
       }
     } else {
-      for (let id of ids[constaintId]) {
-        await Forma.render.removeMesh({ id });
+      if (inScene.includes(constaintId)) {
+        setInScene((inScene) => inScene.filter((id) => id !== constaintId));
+        await Forma.render.remove({ id: constaintId });
       }
     }
 
