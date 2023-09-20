7d82e8143f0b2cc3e72b4beaf81c7d7fbca7e517
knut-sverdrup-adsk <knut.sverdrup@autodesk.com>
2023-09-20 08:27:50 +0200

    fix: target->currentTarget in ResolutionSelector

--- analyses/shadow-study/src/components/ResolutionSelector.tsx
+++ analyses/shadow-study/src/components/ResolutionSelector.tsx
@@ -7,7 +7,7 @@ export default function ResolutionSelector(props: ResolutionSelectorProps) {
   const { resolution, setResolution } = props;
   return (
     <div class="section">
-      <select class="large" value={resolution} onChange={(event) => setResolution(event.target.value)}>
+      <select class="large" value={resolution} onChange={(event) => setResolution(event.currentTarget.value)}>
         <option value="512x384">Small (512x384)</option>
         <option value="1024x768">Medium (1024x768)</option>
         <option value="2048x1536">Large (2048x1536)</option>
