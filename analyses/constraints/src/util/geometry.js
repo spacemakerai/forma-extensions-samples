cad0cbea87e20927fbb5c858a58526cd095e8f9d
Håvard Høiby <havard.hoiby@autodesk.com>
2023-09-21 09:40:35 +0200

    fix: remove optional parameter

--- analyses/constraints/src/util/geometry.js
+++ analyses/constraints/src/util/geometry.js
@@ -1,78 +1,42 @@
 import { Forma } from "https://esm.sh/forma-embedded-view-sdk/auto";
 
 export async function getProposal() {
-  const rootUrn = await Forma.proposal.getRootUrn();
-
   return (
     await Promise.all(
       [
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: "building",
-        })),
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: "generic",
-        })),
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: null,
-        })),
+        ...(await Forma.geometry.getPathsByCategory({ category: "building" })),
+        ...(await Forma.geometry.getPathsByCategory({ category: "generic" })),
+        ...(await Forma.geometry.getPathsByCategory({ category: null })),
       ]
         .filter((path) => path.split("/").length === 2)
-        .map((path) =>
-          Forma.geometry.getTriangles({
-            urn: rootUrn,
-            path,
-          })
-        )
+        .map((path) => Forma.geometry.getTriangles({ path }))
     )
   ).map((a) => Array.from(a));
 }
 
 export async function getSurroundings() {
-  const rootUrn = await Forma.proposal.getRootUrn();
-
   return (
     await Promise.all(
       [
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: "building",
-        })),
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: "generic",
-        })),
-        ...(await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
-          category: null,
-        })),
+        ...(await Forma.geometry.getPathsByCategory({ category: "building" })),
+        ...(await Forma.geometry.getPathsByCategory({ category: "generic" })),
+        ...(await Forma.geometry.getPathsByCategory({ category: null })),
       ]
         .filter((path) => path.split("/").length === 3)
-        .map((path) =>
-          Forma.geometry.getTriangles({
-            urn: rootUrn,
-            path,
-          })
-        )
+        .map((path) => Forma.geometry.getTriangles({ path }))
     )
   ).map((a) => Array.from(a));
 }
 
 export async function getConstraints() {
-  const rootUrn = await Forma.proposal.getRootUrn();
-
   return (
     await Promise.all(
       (
         await Forma.geometry.getPathsByCategory({
-          urn: rootUrn,
           category: "constraints",
         })
       ).map((path) =>
         Forma.geometry.getTriangles({
-          urn: rootUrn,
           path,
         })
       )
@@ -81,17 +45,14 @@ export async function getConstraints() {
 }
 
 export async function getSiteLimits() {
-  const rootUrn = await Forma.proposal.getRootUrn();
-
   const paths = await Promise.all(
     await Forma.geometry.getPathsByCategory({
-      urn: rootUrn,
       category: "site_limit",
     })
   );
 
   const footprints = await Promise.all(
-    paths.map((path) => Forma.geometry.getFootprint({ urn: rootUrn, path }))
+    paths.map((path) => Forma.geometry.getFootprint({ path }))
   );
 
   return footprints.map(({ coordinates }) => coordinates);
