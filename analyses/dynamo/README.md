4583f0489cebf3fb8601911d79baeb25cefb3e2d
Håvard Høiby <havard.hoiby@autodesk.com>
2023-11-28 15:50:32 +0100

    fix: replace dynamo code with reference to new location

---
+++ analyses/dynamo/README.md
@@ -0,0 +1,3 @@
+# Dynamo for Forma Extension
+
+The development of this extension has been moved here: https://github.com/spacemakerai/forma-dynamo-extension
--- analyses/dynamo/src/floatingpanel/pages/components/BlockedView.tsx
+++
@@ -1,9 +0,0 @@
-import dynamoIconUrn from "../../icons/dynamo.png";
-
-export function BlockedView() {
-  return (
-    <div style={{ height: "40px" }}>
-      Please open Dynamo and follow instructions.
-    </div>
-  );
-}
