20a12246dbd6f9a56f3a477a934b91c5eed2091d
Henrik Steen <henrik.steen@autodesk.com>
2025-10-28 09:02:40 +0100

    Update dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,19 @@
 import js from "@eslint/js";
 import prettier from "eslint-plugin-prettier/recommended";
-import hooksPlugin from "eslint-plugin-react-hooks";
 import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   js.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   react.configs.flat.recommended,
   react.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
   {
-    plugins: {
-      "react-hooks": hooksPlugin,
-    },
     settings: {
       react: {
         version: "18",
@@ -40,7 +39,6 @@ export default tseslint.config(
           allowBoolean: true,
         },
       ],
-      ...hooksPlugin.configs.recommended.rules,
       "react/no-unknown-property": "off",
       "react/no-unescaped-entities": "off",
     },
